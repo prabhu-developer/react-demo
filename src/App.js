9d6d8484d08b6563173eedadf3ec9e36f5627336
prabhu-developer <prabhu.k.code@gmail.com>
2024-07-28 10:19:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  useEffect(() => {
+    // api start
+    fetch('https://dummyjson.com/products').then(res => res.json()).then((res) => {
+      setProducts(res.products)
+    })
+    // api end
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        {
+          products.length ?
+            products.map((item) => (
+              <>
+                <li>
+                  <img width={200} src={item.thumbnail} />
+                  {item.title}
+                </li>
+              </>
+            ))
+            : null
+        }
+        <h1>{count}</h1>
+        <button onClick={handleClick}>Add +</button>
+      </center>
     </div>
-  );
+  )
 }
-
-export default App;
